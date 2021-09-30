fc6a9dd84506048302a84fab0c2cfe00619b92fb
abdo hashem <ar1813@fayoum.edu.eg>
2021-09-30 18:01:05 +0200

    lint

--- screens/OtherPage.tsx
+++ screens/OtherPage.tsx
@@ -1,32 +1,24 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from "react-native";
 
-
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../navigation/screens';
-
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/screens";
 
 type Props = NativeStackScreenProps<RootStackParamList, "FirstPage">;
 
-
-
-
 export default function OtherPage({ navigation }: Props) {
-    return (
-        <View style={styles.container}>
-            <Text>i am other page</Text>
-            <Button
-                title="go back"
-                onPress={() => navigation.goBack()}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>i am other page</Text>
+      <Button title="go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-    }
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
 });
