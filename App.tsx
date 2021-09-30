3932027371f9dcd28531c081ed2df1982bf5a671
abdo hashem <ar1813@fayoum.edu.eg>
2021-09-30 21:56:45 +0200

    lint

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigator from './navigation/Navigator'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Navigator from "./navigation/Navigator";
 
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import React from 'react';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import React from "react";
 
 export default function App() {
   return (
@@ -11,7 +11,6 @@ export default function App() {
       <Navigator />
       <StatusBar />
     </SafeAreaProvider>
-
   );
 }
 
--- screens/FirstPage.tsx
+++ screens/FirstPage.tsx
@@ -1,33 +1,27 @@
-import * as React from 'react';
+import * as React from "react";
 
+import { View, Text, Button, StyleSheet } from "react-native";
 
-import { View, Text, Button, StyleSheet } from 'react-native';
-
-
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../navigation/screens';
-
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/screens";
 
 type Props = NativeStackScreenProps<RootStackParamList, "FirstPage">;
 
-
-
-
 export default function FirstPage({ navigation }: Props) {
-    return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to other page"
-                onPress={() => navigation.navigate("OtherPage")}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to other page"
+        onPress={() => navigation.navigate("OtherPage")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-    }
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
 });
