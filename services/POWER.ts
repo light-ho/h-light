606ff04eb3a9e50e286b01738260a458286dfac1
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-10-01 21:11:54 +0200

    fix: make from and to mandatory

--- services/POWER.ts
+++ services/POWER.ts
@@ -5,8 +5,8 @@ export type TimeResolution = "hourly" | "daily" | "weekly" | "monthly";
 export interface POWERApiParams {
   long: string;
   lat: string;
-  from?: Date;
-  to?: Date;
+  from: Date;
+  to: Date;
   resolution?: TimeResolution;
 }
 
