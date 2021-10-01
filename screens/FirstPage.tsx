da21ca58160864a9beba7e1cc2c92d676124db01
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-10-01 14:51:56 +0200

    feat: add victory + example

--- screens/FirstPage.tsx
+++ screens/FirstPage.tsx
@@ -5,9 +5,17 @@ import { View, Text, Button, StyleSheet } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/screens";
 
+import { VictoryChart, VictoryBar, VictoryTheme } from "victory-native";
+
 type Props = NativeStackScreenProps<RootStackParamList, "FirstPage">;
 
 export default function FirstPage({ navigation }: Props) {
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
@@ -15,6 +23,9 @@ export default function FirstPage({ navigation }: Props) {
         title="Go to other page"
         onPress={() => navigation.navigate("OtherPage")}
       />
+      <VictoryChart width={350} theme={VictoryTheme.material}>
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
     </View>
   );
 }
