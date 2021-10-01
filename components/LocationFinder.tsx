1c363ab53631c9f2c967351e218828e1319e98f8
abdo hashem <ar1813@fayoum.edu.eg>
2021-10-01 20:42:47 +0200

    track region change

--- components/LocationFinder.tsx
+++ components/LocationFinder.tsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Text, StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 
+const getInintialPosition = () => {
+  return {
+    latitude: 27.04927786334186,
+    latitudeDelta: 12.895187984301408,
+    longitude: 30.251849088817835,
+    longitudeDelta: 14.525723196566101,
+  }
+};
+
+
+
+
 export default function LocationFinder() {
+
+  const [region, setRegion] = useState(getInintialPosition());
+
+  console.log(region);
+
   return (
     <>
       <Text>i am map</Text>
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
+          region={region}
+          onRegionChangeComplete={(region) => setRegion(region)}
         ></MapView>
       </View>
+      <View style={styles.debugContainer}>
+        <Text>{`current region is \n lat:${region.latitude} \n long:${region.longitude}`}</Text>
+      </View>
+
     </>
   );
 }
@@ -26,9 +43,14 @@ const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     height: 400,
-    width: 400,
     justifyContent: "flex-end",
     alignItems: "center",
+  },
+  debugContainer: {
+    marginTop: 400,
+    marginLeft: 20,
+    position: 'absolute',
+    height: 100,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
