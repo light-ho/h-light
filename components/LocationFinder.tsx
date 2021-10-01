372235f290861d3f7d9a0148d567bb23c7f84070
abdo hashem <ar1813@fayoum.edu.eg>
2021-10-01 16:21:57 +0200

    fix map size to make it apear

--- components/LocationFinder.tsx
+++ components/LocationFinder.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Text } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 import MapView from 'react-native-maps';
 
 
@@ -10,15 +10,32 @@ export default function LocationFinder() {
     return (
         <>
             <Text>i am map</Text>
-            <MapView
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                </MapView>
+            </View>
 
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
