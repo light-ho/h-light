8ef4bf817fda93692f70c7a8c1da613fd36daf75
abdo hashem <ar1813@fayoum.edu.eg>
2021-10-02 22:01:20 +0200

    refine home page

--- utils/asyncStorage.tsx
+++ utils/asyncStorage.tsx
@@ -1,29 +1,28 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const STORAGEKEYS = {
-    location: 'location',
-}
-
-
+  location: "location",
+  solarCost: "solarCost",
+  MonthlyBills: "MonthlyBills",
+};
 
 const storeData = async (key: string, value: any) => {
-    try {
-        await AsyncStorage.setItem(key, JSON.stringify(value))
-    } catch (e) {
-        // saving error
-    }
-}
-
-
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    // saving error
+    console.error("key cannot be stored");
+  }
+};
 
 const getData = async (key: string) => {
-    try {
-        const jsonValue = await AsyncStorage.getItem(key)
-        return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-        // error reading value
-    }
-}
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+    console.error("key not found");
+  }
+};
 
-export { storeData, getData, STORAGEKEYS }
+export { storeData, getData, STORAGEKEYS };
