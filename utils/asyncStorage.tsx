c0cd92e1c039321c86a8be4a8d2c702c8eeb2a4b
abdo hashem <ar1813@fayoum.edu.eg>
2021-10-02 19:10:38 +0200

    lint

--- utils/asyncStorage.tsx
+++ utils/asyncStorage.tsx
@@ -1,29 +1,24 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const STORAGEKEYS = {
-    location: 'location',
-}
-
-
+  location: "location",
+};
 
 const storeData = async (key: string, value: any) => {
-    try {
-        await AsyncStorage.setItem(key, JSON.stringify(value))
-    } catch (e) {
-        // saving error
-    }
-}
-
-
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    // saving error
+  }
+};
 
 const getData = async (key: string) => {
-    try {
-        const jsonValue = await AsyncStorage.getItem(key)
-        return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-        // error reading value
-    }
-}
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
 
-export { storeData, getData, STORAGEKEYS }
+export { storeData, getData, STORAGEKEYS };
